82bf1da144d37cc7048032889257d6b7c7b1c63f
Ubuntu <ubuntu@ip-172-31-5-146.ap-south-1.compute.internal>
2025-12-30 16:33:36 +0000

    Added DockerFile, Jenkins Pipeline code and Kubernetes manifests to repo

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from my DevOps project!!');
+  res.send('Hello from my updated DevOps project!!');
 });
 
 app.listen(3000, () => {
